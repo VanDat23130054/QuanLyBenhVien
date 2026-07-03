d95fd45cfc56cb57fa0a2f40ff282d842e6a2776
IT29-KNK <23130247@st.hcmuaf.edu.vn>
2026-07-03 23:04:43 +0700

    Update BenhNhanMain.java

--- src/main/java/com/httt/quanlybenhvien/BenhNhanMain.java
+++ src/main/java/com/httt/quanlybenhvien/BenhNhanMain.java
@@ -2,7 +2,7 @@ package com.httt.quanlybenhvien;
 
 import com.httt.quanlybenhvien.feature.phucnguyen.PhucNguyenConsoleApp;
 
-public class Main {
+public class BenhNhanMain {
     public static void main(String[] args) {
         new PhucNguyenConsoleApp().run();
     }
