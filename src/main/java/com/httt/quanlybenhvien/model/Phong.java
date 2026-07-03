543d58927fc53ed3b032996b1a50f7adb7499013
Trần Thủy Tứ <23130366@st.hcmuaf.edu.vn>
2026-07-04 05:54:36 +0700

    Update Phong.java

--- src/main/java/com/httt/quanlybenhvien/model/Phong.java
+++ src/main/java/com/httt/quanlybenhvien/model/Phong.java
@@ -1,9 +1,17 @@
 package com.httt.quanlybenhvien.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Phong {
+
     private String maPhong;
 
-    public Phong() {}
+    private List<Giuong> danhSachGiuong = new ArrayList<>();
+    private List<LichTruc> danhSachLichTruc = new ArrayList<>();
+
+    public Phong() {
+    }
 
     public Phong(String maPhong) {
         this.maPhong = maPhong;
@@ -17,10 +25,28 @@ public class Phong {
         this.maPhong = maPhong;
     }
 
+    public List<Giuong> getDanhSachGiuong() {
+        return danhSachGiuong;
+    }
+
+    public void setDanhSachGiuong(List<Giuong> danhSachGiuong) {
+        this.danhSachGiuong = danhSachGiuong;
+    }
+
+    public List<LichTruc> getDanhSachLichTruc() {
+        return danhSachLichTruc;
+    }
+
+    public void setDanhSachLichTruc(List<LichTruc> danhSachLichTruc) {
+        this.danhSachLichTruc = danhSachLichTruc;
+    }
+
     @Override
     public String toString() {
         return "Phong{" +
                 "maPhong='" + maPhong + '\'' +
+                ", soGiuong=" + (danhSachGiuong != null ? danhSachGiuong.size() : 0) +
+                ", soLichTruc=" + (danhSachLichTruc != null ? danhSachLichTruc.size() : 0) +
                 '}';
     }
 }
