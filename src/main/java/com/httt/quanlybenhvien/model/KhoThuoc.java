3d268cbc35de2647c19a07d665eaf2884affab6f
Trần Thủy Tứ <23130366@st.hcmuaf.edu.vn>
2026-07-04 05:46:10 +0700

    Update KhoThuoc.java

--- src/main/java/com/httt/quanlybenhvien/model/KhoThuoc.java
+++ src/main/java/com/httt/quanlybenhvien/model/KhoThuoc.java
@@ -1,14 +1,22 @@
 package com.httt.quanlybenhvien.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class KhoThuoc {
+
     private String maKhoThuoc;
-    private String maDuocSi; // references NhanVien.maNhanVien
 
-    public KhoThuoc() {}
+    private NhanVien duocSi;
 
-    public KhoThuoc(String maKhoThuoc, String maDuocSi) {
+    private List<Thuoc> danhSachThuoc = new ArrayList<>();
+
+    public KhoThuoc() {
+    }
+
+    public KhoThuoc(String maKhoThuoc, NhanVien duocSi) {
         this.maKhoThuoc = maKhoThuoc;
-        this.maDuocSi = maDuocSi;
+        this.duocSi = duocSi;
     }
 
     public String getMaKhoThuoc() {
@@ -19,19 +27,28 @@ public class KhoThuoc {
         this.maKhoThuoc = maKhoThuoc;
     }
 
-    public String getMaDuocSi() {
-        return maDuocSi;
+    public NhanVien getDuocSi() {
+        return duocSi;
     }
 
-    public void setMaDuocSi(String maDuocSi) {
-        this.maDuocSi = maDuocSi;
+    public void setDuocSi(NhanVien duocSi) {
+        this.duocSi = duocSi;
+    }
+
+    public List<Thuoc> getDanhSachThuoc() {
+        return danhSachThuoc;
+    }
+
+    public void setDanhSachThuoc(List<Thuoc> danhSachThuoc) {
+        this.danhSachThuoc = danhSachThuoc;
     }
 
     @Override
     public String toString() {
         return "KhoThuoc{" +
                 "maKhoThuoc='" + maKhoThuoc + '\'' +
-                ", maDuocSi='" + maDuocSi + '\'' +
+                ", duocSi=" + (duocSi != null ? duocSi.getMaNhanVien() : null) +
+                ", soThuoc=" + (danhSachThuoc != null ? danhSachThuoc.size() : 0) +
                 '}';
     }
 }
